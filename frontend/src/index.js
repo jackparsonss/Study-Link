100098c2472a1488aa6be18742b20be017c71c5c
RyderCatonio <rydercatonio8@gmail.com>
2022-02-19 12:31:06 -0700

    Bad message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Components/Login";
+import Login from "./Components/Login/Login";
 
 ReactDOM.render(
   <React.StrictMode>
