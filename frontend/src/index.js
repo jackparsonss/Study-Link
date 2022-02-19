5b01b166a194a98bbdad45e81d9cb8ebb6ef95e4
Jack Parsons <jackpar1113@gmail.com>
2022-02-19 12:27:29 -0700

    fix react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
 
 ReactDOM.render(
   <React.StrictMode>
