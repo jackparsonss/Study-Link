5a440eeaa44d7e48623af3dac092ee647481ed1b
Jack Parsons <jackpar1113@gmail.com>
2022-02-19 19:58:00 -0700

    logout by hitting settings button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import NavBar from "./Components/Navbar/NavBar";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedIn = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        return navigate("/signup");
+      }
+    });
+    return loggedIn;
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
--- frontend/src/Components/Navbar/NavBar.js
+++ frontend/src/Components/Navbar/NavBar.js
@@ -2,44 +2,33 @@ import React from "react";
 import "./NavBar.css";
 import { FaHome } from "react-icons/fa";
 import { HiChat } from "react-icons/hi";
-import { FiSettings } from "react-icons/fi"
-import { GiNotebook } from "react-icons/gi"
-
-
-
-
+import { FiSettings } from "react-icons/fi";
+import { GiNotebook } from "react-icons/gi";
 
 function NavBar() {
-  return <div className="nav">
+  return (
+    <div className="nav">
+      <div className="NavItem Home">
+        <FaHome className="NavIcon" />
+        <p>Study Link</p>
+      </div>
 
-    <div className="NavItem Home">
-      <FaHome className="NavIcon" />
-      <p>Study Link</p>
-    </div>
+      <div className="NavItem">
+        <GiNotebook className="NavIcon" />
+        <p>Classes</p>
+      </div>
 
-    <div className="NavItem">
-      <GiNotebook className="NavIcon" />
-      <p>Classes</p>
-    </div>
+      <div className="NavItem">
+        <HiChat className="NavIcon" />
+        <p>Chat</p>
+      </div>
 
-    <div className="NavItem">
-      <HiChat className="NavIcon" />
-      <p>Chat</p>
+      <div className="NavItem">
+        <FiSettings className="NavIcon" />
+        <p>Settings</p>
+      </div>
 
-    </div>
-
-    <div className="NavItem">
-      <FiSettings className="NavIcon" />
-      <p>Settings</p>
-    </div>
-
-
-
-
-
-
-
-    {/* <ul class="nav__links">
+      {/* <ul class="nav__links">
       <li><a className="studylinks" href="#1">StudyLinks</a></li>
       <li>
         <div className="Classes">
@@ -59,7 +48,8 @@ function NavBar() {
       <li><a href="#3"><HiChat />Chat</a></li>
       <li><a href="#4"><FiSettings />Settings</a></li>
     </ul> */}
-  </div>;
+    </div>
+  );
 }
 
 export default NavBar;
